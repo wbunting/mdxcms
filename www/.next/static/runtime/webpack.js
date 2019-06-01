6982b0adddd02e20c67621e0d2619fdf4674eab1
Will <bunting.will@gmail.com>
2019-06-01 20:25:13 +0900

    better loading indicator

--- www/.next/static/runtime/webpack.js
+++ www/.next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d6c9fd68a301c0886301";
+/******/ 	var hotCurrentHash = "740522587146c8d8c0ea";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
